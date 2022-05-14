9edcbe9384eb9e82fef1cbadc0dad5e37c67aa8c
Rengin Özgün Akbor <47036671+renginozgun@users.noreply.github.com>
2022-05-14 23:31:28 +0300

    The Kid Basic Movement

--- Build A Family/Assets/Scripts/KidMovement.cs
+++ Build A Family/Assets/Scripts/KidMovement.cs
@@ -7,53 +7,58 @@ using UnityEngine;
 
 public class KidMovement : MonoBehaviour
 {
-    public CharacterController controller;
-    public Transform cam;
+  // public Animator playerAnim;
+	public Rigidbody playerRigid;
+	public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+	public bool walking;
+	public Transform playerTrans;
+	
+	
+	void FixedUpdate(){
+		if(Input.GetKey(KeyCode.W)){
+			playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			playerRigid.velocity = -transform.forward * wb_speed * Time.deltaTime;
+		}
+	}
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.W)){
+            Debug.Log("W'ye bastı");
+			walking = true;
+		
+		}
+		if(Input.GetKeyUp(KeyCode.W)){
+            Debug.Log("W'den kaldırdı");
+			walking = false;
+	
+		}
+		if(Input.GetKeyDown(KeyCode.S)){
+	
+		
+		}
+		if(Input.GetKeyUp(KeyCode.S)){
 
-    public float speed = 6;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3;
-    Vector3 velocity;
-    bool isGrounded;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
+		}
 
-    float turnSmoothVelocity;
-    public float turnSmoothTime = 0.1f;
-
-    // Update is called once per frame
-    void Update()
-    {
-        //jump
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
+		if(walking == true){				
+			if(Input.GetKeyDown(KeyCode.LeftShift)){
+				w_speed = w_speed + rn_speed;
+			}
+			if(Input.GetKeyUp(KeyCode.LeftShift)){
+				w_speed = olw_speed;
+			
+			}
+		}else{
+            playerRigid.velocity = new Vector3(0,0,0);
         }
+	}
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-        }
-        //gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-        //walk
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-    }
 }
