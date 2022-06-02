c3ef50a8ed5c6be139e9b2e04c2426e6f2d9011c
Rengin Özgün Akbor <47036671+renginozgun@users.noreply.github.com>
2022-06-02 21:51:38 +0300

    Dad Y Axis Position Fix

--- Build A Family/Assets/Scripts/Npc/NpcPathWalk.cs
+++ Build A Family/Assets/Scripts/Npc/NpcPathWalk.cs
@@ -7,13 +7,12 @@ public class NpcPathWalk : MonoBehaviour
     
     [SerializeField] GameObject Jake;
 
-     private Vector3 initialPivotPosition = new Vector3(-26.04f, -0.2332252f, -0.85f); //0
+     private Vector3 initialPivotPosition = new Vector3(-26.04f, 0.2973232f, -0.85f); //0
 
-    private Vector3 firstPivotPosition = new Vector3(4.56f, -0.2332252f, 1.01f); //1+
+    private Vector3 firstPivotPosition = new Vector3(4.56f, 0.2973232f, 1.01f); //1+
 
-    private Vector3 secondPivotPosition = new Vector3(-15, -0.2332252f, 11); //2 +
+    private Vector3 secondPivotPosition = new Vector3(-15, 0.2973232f, 11); //2 +
 
-    private Vector3 thirdPivotPosition = new Vector3(-9, 0, -5); //3
 
     private Vector3 beginning = new Vector3(-9.64f, -1, 16.21f);
 
