c399f55e14a5e104b24ca5ba460fe871d20fae13
Rengin Özgün Akbor <47036671+renginozgun@users.noreply.github.com>
2022-06-03 19:33:50 +0300

    Sliders and UI Changes

---
+++ Build A Family/Assets/Beautiful Progress Bar Free - CYKO/Scripts/SliderScripts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class SliderScripts : MonoBehaviour
+{
+    public Slider slider;
+    public Image fill;
+
+    private void Start()
+    {
+        FillSlider();
+    }
+
+    public void FillSlider()
+    {
+        slider.value=5;
+        fill.fillAmount = slider.value;
+    }
+}
--- Build A Family/Assets/Scripts/Menu/Settings.cs
+++ Build A Family/Assets/Scripts/Menu/Settings.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    [SerializeField]
-    public Button btn;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        btn.onClick.AddListener(onButtonClick);
+
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+      if(Input.GetKey(KeyCode.Escape)){
+          onButtonClick();
+      }
+    }
 
-  void onButtonClick(){
-   
-    SceneManager.LoadScene("Pause Menu");
+    void onButtonClick()
+    {
+        SceneManager.LoadScene("Pause Menu");
     }
 }
